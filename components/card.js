ddeacb4b7f2cb8206eedf81bfad31da11f36c018
Om Jawade <omjawade08@gmail.com>
2021-09-12 08:47:05 +0530

    Stylling Better

--- components/card.js
+++ components/card.js
@@ -21,7 +21,7 @@ const Card = ({ type, title, value, link }) => {
           padding: 1.5rem;
           text-align: left;
           color: inherit;
-          text-decoration: none;
+          text-decoration:  none;
           border: 1px solid #eaeaea;
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
